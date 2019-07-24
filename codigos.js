712fa11e020dd61b70204af14fbeaa19ef4e45f0
Saulo Matos <saulomm15@gmail.com>
2019-07-24 16:13:13 -0300

    Primeira tela criada

---
+++ codigos.js
@@ -0,0 +1,17 @@
+function mudarImagem1(){
+    document.getElementById('pedra').src='imagens/Pedra_ok.jpg'
+    document.getElementById('papel').src='imagens/Papel.jpg'
+    document.getElementById('tesoura').src='imagens/Tesoura.jpg'
+}
+
+function mudarImagem2(){
+    document.getElementById('papel').src='imagens/Papel_ok.jpg'
+    document.getElementById('tesoura').src='imagens/Tesoura.jpg'
+    document.getElementById('pedra').src='imagens/Pedra.jpg'
+}
+
+function mudarImagem3(){
+    document.getElementById('tesoura').src='imagens/Tesoura_ok.jpg'
+    document.getElementById('pedra').src='imagens/Pedra.jpg'
+    document.getElementById('papel').src='imagens/Papel.jpg'
+}
